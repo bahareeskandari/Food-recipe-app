d19070e4be971395d700b4d9f341fdf3ef929260
bahareeskandari <bahareeskandarijou@gmail.com>
2020-07-21 10:16:08 +0200

    testing with require

--- src/components/util/yelp.js
+++ src/components/util/yelp.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 
-const apiKey =
-  'RYZQu4qcpmV92yqbxWx2AjBqO0J4iNcO6d-_XysNW7w6GrnyBTYdDuv8zqD6eic26Nvfks2VwZ4YPmYNgDX2_yysfq6PyOxFZsG8dldDsVfv6gyLCiI_WUXoa5CDXnYx'
-
-const yelp = {
-  searchYelp(term, location, sortBy) {
+let KEYS
+if (process.env.NODE_ENV === 'production') {
+  KEYS = process.env
+} else {
+  KEYS = require('../util/keys.json')
+}
+const yelp =  {
+  async searchYelp(term, location, sortBy) {
     return fetch(
       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-      {headers: {Authorization: `Bearer ${apiKey}`}}
+      {headers: {Authorization: `Bearer ${KEYS.apiKey}`}}
     )
       .then((response) => response.json())
       .then((jsonResponse) => {
